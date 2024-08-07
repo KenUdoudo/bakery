6f2bfb2fd54c7233a1be75cc728543aef8ed9f13
Ken Udoudo <kendo302003@gmail.com>
2024-08-07 07:39:46 +0100

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,15 +5,15 @@ import { TbMinusVertical } from "react-icons/tb";
 import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
     return ( 
-        <div className='w-full flex flex-row justify-between px-[40px]'>
-            <h2>Bakery</h2>
-            <div>
+        <div className='w-full flex flex-row justify-between items-center px-[50px] pt-[20px]'>
+            <h2 className="text-[34px] font-semibold">Bakery</h2>
+            <div className="flex flex-row gap-[30px]">
                 <a>manufacturing</a>
                 <a>packaging</a>
                 <a>farms</a>
                 <a>delivery</a>
             </div>
-            <div>
+            <div className="flex flex-row gap-[30px]">
                 <FiSearch className="w-[20px] h-[20px]" />
                 <GoPerson className="w-[20px] h-[20px]" />
                 <TfiShoppingCart className="w-[20px] h-[20px]" />
