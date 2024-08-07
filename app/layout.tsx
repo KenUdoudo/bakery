ac103248ee80709234ea08b081dd33dfc83c90c5
Ken Udoudo <kendo302003@gmail.com>
2024-08-07 07:36:50 +0100

    imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const poppins = Poppins({ subsets: ["latin"],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -17,7 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="max-w-[1366px]">
+      <Navbar />
       <body className={poppins.className}>{children}</body>
     </html>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,25 @@
+import { FiSearch } from "react-icons/fi";
+import { GoPerson } from "react-icons/go";
+import { TfiShoppingCart } from "react-icons/tfi";
+import { TbMinusVertical } from "react-icons/tb";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () => {
     return ( 
-        <div>
+        <div className='w-full flex flex-row justify-between px-[40px]'>
             <h2>Bakery</h2>
             <div>
                 <a>manufacturing</a>
                 <a>packaging</a>
                 <a>farms</a>
                 <a>delivery</a>
+            </div>
+            <div>
+                <FiSearch className="w-[20px] h-[20px]" />
+                <GoPerson className="w-[20px] h-[20px]" />
+                <TfiShoppingCart className="w-[20px] h-[20px]" />
+                <p>$225.00</p>
+                <TbMinusVertical className="w-[20px] h-[20px]" />
+                <RxHamburgerMenu className="w-[20px] h-[20px]" />
             </div>
         </div>
     );
