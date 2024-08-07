a213dc17e215820ee74f1d8d463acee5ab9f457c
Ken Udoudo <kendo302003@gmail.com>
2024-08-07 07:19:52 +0100

    creaning navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div>
+            <h2>Bakery</h2>
+            <div>
+                <a>manufacturing</a>
+                <a>packaging</a>
+                <a>farms</a>
+                <a>delivery</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
