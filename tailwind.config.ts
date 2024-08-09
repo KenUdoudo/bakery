806982e4fe31124adc1618b23879bef920c992ae
Ken Udoudo <kendo302003@gmail.com>
2024-08-09 08:54:01 +0100

    screen edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ const config: Config = {
       fontFamily: {
         pop: ["Archivo", "system-ui" ],
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1440px',
+      },
     },
   },
   plugins: [],
