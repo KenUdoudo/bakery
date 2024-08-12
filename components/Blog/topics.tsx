e2743cb8f25019c89e5c9162a32aad32a1199f27
Ken Udoudo <kendo302003@gmail.com>
2024-08-12 09:24:02 +0100

    topics

--- components/Blog/topics.tsx
+++ components/Blog/topics.tsx
@@ -11,7 +11,7 @@ const Topics = () => {
                 <p className="p-[10px] border-[1px] border-[#000] rounded-[50px] font-[700] text-[14px] tracking-[-0.42px] cursor-pointer">Academics</p>
             </div>
 
-            <div className="flex justify-between items-start mt-[100px]">
+            <div className="flex justify-between items-start gap-[24px] mt-[100px]">
                 <div className="flex flex-col items-start self-stretch gap-[15px]">
                     <Image 
                         src='/blogassets/unlock.png'
@@ -42,7 +42,58 @@ const Topics = () => {
                 </div>
 
                 <div className="flex flex-col justify-between items-center self-stretch">
-                    
+                    <div className="flex gap-[24px] items-center">
+                        <Image 
+                            src='/blogassets/maximize.png'
+                            width={194}
+                            height={142}
+                            quality={100}
+                            alt=""
+                        />
+                        <div className="flex flex-col gap-[35px] justify-center items-start self-stretch">
+                            <p className="text-[25px] font-[700] tracking-[-0.75px] leading-[27px]">Maximizing your productivity for academic excellence</p>
+                            <p className="text-[18px] font-[400] tracking-[-0.54px]">John Okafor</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-[24px] items-center">
+                        <Image 
+                            src='/blogassets/how.png'
+                            width={194}
+                            height={142}
+                            quality={100}
+                            alt=""
+                        />
+                        <div className="flex flex-col gap-[35px] justify-center items-start self-stretch">
+                            <p className="text-[25px] font-[700] tracking-[-0.75px] leading-[27px]">How to be a force to reckon with at hackathons</p>
+                            <p className="text-[18px] font-[400] tracking-[-0.54px]">Matthew Johnson</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-[24px] items-center">
+                        <Image 
+                            src='/blogassets/will.png'
+                            width={194}
+                            height={142}
+                            quality={100}
+                            alt=""
+                        />
+                        <div className="flex flex-col gap-[35px] justify-center items-start self-stretch">
+                            <p className="text-[25px] font-[700] tracking-[-0.75px] leading-[27px]">Will my department be the end of me?</p>
+                            <p className="text-[18px] font-[400] tracking-[-0.54px]">Nathaniel Chukwuka</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-[24px] items-center">
+                        <Image 
+                            src='/blogassets/surviving.png'
+                            width={194}
+                            height={142}
+                            quality={100}
+                            alt=""
+                        />
+                        <div className="flex flex-col gap-[35px] justify-center items-start self-stretch">
+                            <p className="text-[25px] font-[700] tracking-[-0.75px] leading-[27px]">Surviving MTU</p>
+                            <p className="text-[18px] font-[400] tracking-[-0.54px]">Jessica Adetola</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
